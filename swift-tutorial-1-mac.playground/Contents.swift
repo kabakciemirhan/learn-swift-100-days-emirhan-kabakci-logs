7dd064bf60894e21db71f777ec7cb65d2ccbd411
Emirhan Kabakcı <kabakcemirhan@gmail.com>
2023-12-12 21:14:05 +0300

    first playgrounds has started
    let, var and print

---
+++ swift-tutorial-1-mac.playground/Contents.swift
@@ -0,0 +1,6 @@
+import Cocoa
+
+let sayi1 = 3
+let sayi2 = 6
+let toplam = sayi1 + sayi2
+print(toplam)
---
+++ swift-tutorial1.playground/Contents.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+var greeting = "Hello, playground"
+var sayi1 = 2
+var sayi2 = 5
+var toplam = sayi1 + sayi2
+
+print(toplam)
